db4e06bea1134608cb8df4368ce78666e4f3ed7c
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-05-01 02:58:10 +0200

    add spriteWrap option

--- src/options.js
+++ src/options.js
@@ -1,27 +1,32 @@
-module.exports = {
+/**
+ * @type {import("..").IOptions}
+ */
+const options = {
   path: "",
   globalClasses: "",
   defaultClasses: "",
   svgSpriteShortcode: "svgsprite",
   svgShortcode: "svg",
-  spriteConfig:
-  {
+  spriteWrap: null, // defaults to { height: '0', width: '0' }
+  spriteConfig: {
     mode: {
       inline: true,
       symbol: {
-        sprite: 'sprite.svg',
+        sprite: "sprite.svg",
         example: false,
       },
     },
     shape: {
-      transform: ['svgo'],
+      transform: ["svgo"],
       id: {
-        generator: 'svg-%s',
+        generator: "svg-%s",
       },
     },
     svg: {
       xmlDeclaration: false,
       doctypeDeclaration: false,
     },
-  }
+  },
 };
+
+module.exports = options;
