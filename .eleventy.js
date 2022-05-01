053a5127629dc4d76e11113d75c69f01bdba80b4
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-05-01 02:27:40 +0200

    omit newlines in the template literal

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const path = require('path');
 const SVGSprite = require("./src/SVGSprite");
 
 module.exports = (eleventyConfig, options = {}) => {
+
   if (!options.path) {
     throw new Error("[eleventy-plugin-svg-sprite] path must be specified in plugin options");
   }
@@ -10,25 +11,26 @@ module.exports = (eleventyConfig, options = {}) => {
   let config = Object.assign(defaultOptions, options);
 
   let svgSpriteInstance = new SVGSprite(config);
+
   eleventyConfig.on('beforeBuild', async () => { await svgSpriteInstance.compile(config); });
 
   eleventyConfig.addShortcode(config.svgSpriteShortcode, svgSpriteInstance.getSvgSprite);
 
   eleventyConfig.addShortcode(config.svgShortcode, (name, classes, desc, location) => {
-    // "desc" and "location" attributes are required for accessibility and 
-    // Lighthouse validations and are hardcoded in the layouts to provide 
+
+    // "desc" and "location" attributes are required for accessibility and
+    // Lighthouse validations and are hardcoded in the layouts to provide
     // unique values as required by Lighthouse.
     if (!name) {
       throw new Error("[eleventy-plugin-svg-sprite] name of SVG must be specified");
     }
+
     const nameAttr = name;
     const classesAttr = `${config.globalClasses} ${classes || config.defaultClasses}`;
     const descAttr = desc || `${nameAttr} icon`;
     const locationAttr = location || 'content';
 
-    return `<svg class="${classesAttr}" aria-describedby="symbol-${nameAttr}-desc" aria-labelledby="symbol-${nameAttr}-desc" role="group">
-                <desc id="symbol-${nameAttr}-desc-${locationAttr}">${descAttr}</desc>
-                <use xlink:href="#svg-${nameAttr}"></use>
-            </svg>`;
+    return `<svg class="${classesAttr}" aria-describedby="symbol-${nameAttr}-desc" aria-labelledby="symbol-${nameAttr}-desc" role="group"><desc id="symbol-${nameAttr}-desc-${locationAttr}">${descAttr}</desc><use xlink:href="#svg-${nameAttr}"></use></svg>`;
+
   });
 };
