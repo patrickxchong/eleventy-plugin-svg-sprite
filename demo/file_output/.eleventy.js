e04841cbfabf0bb857410c2b0f3c5d4eccf9a162
Patrick Chong <patrickxchong@gmail.com>
2022-11-10 19:34:57 +0800

    docs: Add file output demo

---
+++ demo/file_output/.eleventy.js
@@ -0,0 +1,10 @@
+const svgSprite = require("eleventy-plugin-svg-sprite");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(svgSprite, {
+    path: "./assets/svg",
+    globalClasses: "svgicon",
+    defaultClasses: "default-class",
+    outputFilepath: "./_site/sprites/icons.svg"
+  });
+};
---
+++ demo/file_output/demo-11ty.11ty.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+  return `<div>
+    <svg class="">
+      <use xlink:href="/sprites/icons.svg#svg-placeholder-feature"></use>
+    </svg>
+    <svg class="">
+      <use xlink:href="/sprites/icons.svg#svg-placeholder-slider"></use>
+    </svg>
+    <svg class="">
+      <use xlink:href="/sprites/icons.svg#svg-placeholder-thumb"></use>
+    </svg>
+  </div>`;
+};
